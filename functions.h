c7817ddd17a01d13112ac1bbaf700dafa08437d2
siveva <reyan071017@gmail.com>
2023-09-20 14:37:18 +0800

    backup for edges

--- functions.h
+++ functions.h
@@ -51,6 +51,52 @@ void AssignNames(GraphNode **head)
     }
 }
 
+void SetEdges(GraphNode *head)
+{
+    GraphNode *node1 = head;
+    GraphNode *node2 = node1->next;
+    GraphNode *node3 = node2->next;
+    GraphNode *node4 = node3->next;
+    GraphNode *node5 = node4->next;
+
+    Edge *edge = (Edge*)malloc(sizeof(Edge));
+    edge->destination = node2;
+    edge->distance = 4;
+    edge->next = node1->edges;
+    node1->edges = edge;
+
+    Edge *edge2 = (Edge*)malloc(sizeof(Edge));
+    edge2->destination = node3;
+    edge2->distance = 3;
+    edge2->next = node2->edges;
+    node2->edges = edge2;
+
+    Edge *edge3 = (Edge*)malloc(sizeof(Edge));
+    edge3->destination = node4;
+    edge3->distance = 5;
+    edge3->next = node2->edges;
+    node2->edges = edge3;
+
+    Edge *edge4 = (Edge*)malloc(sizeof(Edge));
+    edge4->destination = node5;
+    edge4->distance = 3;
+    edge4->next = node3->edges;
+    node3->edges = edge4;
+
+    Edge *edge5 = (Edge*)malloc(sizeof(Edge));
+    edge5->destination = node3;
+    edge5->distance = 2;
+    edge5->next = node4->edges;
+    node4->edges = edge5;
+
+    Edge *edge6 = (Edge*)malloc(sizeof(Edge));
+    edge6->destination = node5;
+    edge6->distance = 4;
+    edge6->next = node4->edges;
+    node4->edges = edge6;
+    
+}
+
 void DisplayNode(GraphNode *head)
 {
     GraphNode *current = head;
@@ -59,6 +105,22 @@ void DisplayNode(GraphNode *head)
         puts(current->name);
         current = current->next;
     }
+}
+
+void DisplayEdges(GraphNode *head)
+{
+    GraphNode *current = head;
+    while(current)
+    {
+        Edge *edge = current->edges;
+        while(edge)
+        {
+            printf("From %s to %s, distance = %d\n", current->name, edge->destination->name, edge->distance);
+            edge = edge->next;
+        }
+        current = current->next;
+    }
+
 }
 
 void AskStartingNode(char startNodeName[])
@@ -105,6 +167,8 @@ void GraphOperations(GraphNode *head, Edge *edge)
         {
             case 1:
                   AssignNames(&head);
+                  SetEdges(head);
+                  DisplayEdges(head);
                   //DisplayNode(head);
                   break;
             case 2:
